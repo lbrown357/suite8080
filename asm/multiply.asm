39ba14631d821948d53955c932adb10f049bf467
lbrown357 <lmb20689@email.vccs.edu>
2022-11-27 19:20:57 -0500

    Finish multiply.asm with stdlib.asm

--- asm/multiply.asm
+++ asm/multiply.asm
@@ -4,20 +4,19 @@ INCLUDE stdlib.asm
 
         ORG    0h       ; Set Program Counter to address 0
 START:  LDA    VAL1     ; Load value (5) at VAL1 (200) into Accumulator
-        MOV    B,A      ; Move value in Accumulator to Register B
+        MOV    C,A      ; Move value in Accumulator to Register B
         LDA    VAL2     ; Load value (8) at VAL2 (201) into Accumulator
-        LXI    BC, VAL1
-        LXI    DE, VAL2
+        MOV    D,A      ; Move value in Accumulator to Register B
         CALL   MULTI
-        POP    BC
-        MVI    C,SUM
-        JMP    START    ; Jump to start of code (infinite loop)
+        MOV    A,C
+        STA    SUM
+        HLT    ; Jump to start of code (infinite loop)
 
 ; Data segment:
 
         ORG    80h     ; Set Program Counter to address 200
-VAL1:   DB     5h       ; Data Byte at address 200 = 5
-VAL2:   DB     8h      ; Data Byte at address 201 = 8 (10 octal)
+VAL1:   DB     7h       ; Data Byte at address 200 = 5
+VAL2:   DB     6h      ; Data Byte at address 201 = 8 (10 octal)
 SUM:    DB     0h       ; Data Byte at address 202 = 0
 
 
